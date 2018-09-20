6602295c7af6f984725d8216942f4c93c394df71
yuminchoi <chldbals219@naver.com>
2018-09-20 16:30:00 +0900

    02 02

--- first.c
+++ first.c
@@ -4,15 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int input_int;
-	float input_float;
+	char c;
+	int i;
 	
-	printf("Enter an integer: ");
-	scanf("%d", &input_int);
+	printf("Input a number: ");
+	scanf("%c", &c);
 	
-	printf("Enter an float: ");
-	scanf("%f", &input_float);
-	
-	printf("integer:%d, float:%f\n", input_int, input_float);
+	i=c-'0';
+	printf("The input number is %i\n", i);
 	return 0;
 }
